df3c37f80a10f569f7993929b118438209e3dbca
Ngan Do <ngobap20001710@gmail.com>
2019-09-06 23:52:37 -0400

    first púh

---
+++ ARTextbook/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ARTextbook
+//
+//  Created by Ngan Do on 9/6/19.
+//  Copyright © 2019 Ngan Do. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.performSegue(withIdentifier: "loginView", sender: self)
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
